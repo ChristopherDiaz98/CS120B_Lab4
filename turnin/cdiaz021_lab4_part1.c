3641b0aef8c36db1434d8832b6562b0b255ec186
Christopher Diaz <cdiaz021@wch136-13.cs.ucr.edu>
2019-10-11 17:28:07 -0700

    working on part 1

---
+++ turnin/cdiaz021_lab4_part1.c
@@ -0,0 +1,65 @@
+/*	Author: cdiaz021
+ *  Partner(s) Name: Jacob Halvorson
+ *	Lab Section:
+ *	Assignment: Lab 4  Exercise 1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00;	PORTA = 0xFF;
+    DDRB = 0xFF;	PORTB = 0x00; 
+
+    /* Insert your solution below */
+    unsigned char tmp = 0x00;
+    unsigned char LED_0 = 0x00;
+    unsigned char LED_1 = 0x00;
+    enum States {start,B0_ON,B1_ON} state;
+
+    while (1) {
+
+	    tmp = PINA & 0xFF;
+	    LED_0 = 0x00;
+	    LED_1 = 0x00;
+
+	    switch(state){
+
+		    case start:
+			    state = B0_ON;
+			    break;
+		    case B0_ON:
+			    if(tmp == 0x01) {state = B1_ON;}
+			    else {state = B0_ON;}
+			    break;
+		    case B1_ON:
+			    if(tmp == 0x01) {state = B0_ON;}
+			    else {state = B1_ON;}
+			    break;
+		    default:
+			    state = B0_ON;
+			    break;
+	    }
+
+	    switch(state){
+	
+		    case B0_ON:
+			    LED_0 = 0x01;
+			    LED_1 = 0x00;
+			    break;
+		    case B1_ON:
+			    LED_0 = 0x00;
+			    LED_1 = 0x02;
+			    break;
+	    }
+
+	    PORTB = LED_0 | LED_1;
+    }
+    return 1;
+}
