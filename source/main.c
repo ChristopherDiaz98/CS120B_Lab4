d7af481320feececaa88434439e0bc63be8a89ce
ChristopherDiaz98 <43664545+ChristopherDiaz98@users.noreply.github.com>
2019-10-12 19:58:08 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -12,82 +12,58 @@
 #include "simAVRHeader.h"
 #endif
 
+unsigned char tmpA;
+unsigned char tmpB;
+enum States {LOCKED, POUND, UNLOCKED} state;
+
+void tick() {
+    
+    tmpA = 0xFF & PINA;
+
+    switch (state) { //state transitions
+	case LOCKED:
+	 	if (tmpA == 0x04) {state = POUND;}
+		else {state = LOCKED;}
+		break;
+	case POUND:
+		if (tmpA == 0x02) {state = UNLOCKED;}
+		else if (tmpA == 0x00) {state = POUND;}
+		else {state = LOCKED;}
+		break;
+	case UNLOCKED:
+		if (tmpA == 0x80) {state = LOCKED;}
+		else {state = UNLOCKED;}
+		break;
+	default:
+		state = LOCKED;
+		break;
+		
+    }
+    switch (state) {
+        case LOCKED:
+		tmpB = 0x00;		
+                break;
+        case POUND:
+                break;
+        case UNLOCKED:
+		tmpB = 0x01;
+                break;
+        default:
+                break;
+    }	    	
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-    DDRA = 0x00;	PORTA = 0xFF;
-    DDRC = 0xFF;	PORTC = 0x00; 
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00; 
 
     /* Insert your solution below */
-    unsigned char tmp = 0x00;
-    unsigned char cnt = 0x00;
-    unsigned char inc = 0x00;
-    unsigned char dec = 0x00;
-    enum States {start,increment,decriment,wait,reset} state;
+    state = LOCKED;  
 
     while (1) {
-
-	    tmp = PINA & 0xFF;
-	    inc = 0x00;
-	    dec = 0x00;
-
-	    switch(state){
-
-		    case start:
-			    if(tmp == 0x01) {state = increment;}
-			    else if(tmp == 0x03) {state = reset;}
-			    else {state = wait;}
-		    case increment:
-			    if(tmp = 0x01) {state = increment;}
-			    else if(tmp == 0x02) {state = decrement;}
-			    else if(tmp == 0x03) {state = reset;}
-			    else {state = wait;}
-			    break;
-		    case decrement:
-			    if(tmp == 0x01) {state = increment;}
-			    else if (tmp == 0x02) {state = decrement;}
-			    else if(tmp == 0x03) {state = reset;}
-			    else (state = wait;)
-			    break;
-		    case wait:
-			    if(tmp == 0x01) {state = increment;}
-                            else if(tmp == 0x02) {state = decrement;}
-                            else if(tmp == 0x03) {state = reset;}
-			    else {state = wait;}
-                            break;
-		    case reset:
-			    if(tmp == 0x01) {state = increment;}
-                            else if(tmp == 0x02) {state = decrement;}
-                            else if(tmp == 0x03) {state = reset;}
-			    else {state = wait;}
-                            break;
-		    default:
-			    state = wait;
-			    break;
-	    }
-
-	    switch(state){
-	
-		    case :
-			    LED_0 = 0x01;
-			    LED_1 = 0x00;
-			    break;
-		    case waitB0:
-			    LED_0 = 0x01;
-			    LED_1 = 0x01;
-			    break;
-		    case B1_ON:
-			    LED_0 = 0x00;
-			    LED_1 = 0x02;
-			    break;
-		    case waitB1:
-			    LED_0 = 0x00;
-			    LED_1 = 0x02;
-			    break;
-		    default:
-			    break;
-	    }
-
-	    PORTC = LED_0 | LED_1;
+	tick();
+	PORTB = tmpB;
     }
     return 1;
 }
